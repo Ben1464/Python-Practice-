88d2c664e55335a354376d69ff99a6376fe44df0
Benson Mwangi <kimaniben@gmail.com>
2024-01-11 12:08:08 +0000

    Initial commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+def hello():
+    print("Hello world!")
+
+hello()
+
+list_courses= ["SE","DS", "DevOPS"]
+print("list_courses", list_courses)
+
+name = "Ben"
+age =28
+course = "Software enginerring"
+school = "Moringa school"
+print(f"My name is {name} i am {age} old and i am currently learing {course} at {school}")
+
+price_1=250
+price_2=400
+print(f"The shirt cost Ksh {price_1} and the lunch consted us Ksh {price_2}")
