6aa71fea788ac048d6eab50c8971108377cbf8b5
Benson Mwangi <kimaniben@gmail.com>
2024-01-15 06:15:07 +0000

    Python data structures practice

--- hello.py
+++ hello.py
@@ -25,3 +25,39 @@ def stylish_painter ():
     best_hairstyle = "kinky"
     return "Micheal Jackson"
 stylish_painter()
+#Example of a sequence 
+my_list =[1,2,3,4,5]
+print (my_list[2])
+print (my_list[3])
+#Example of a tuple
+my_tuple = (3,79,"Alex",6.2,"James")
+print(my_tuple[3])
+print(my_tuple[0:3])
+#Sequence operations
+s = [3,5,9,0,12,17.3]
+17.3 in s #This will return "true"
+#Python list and its methonds 
+my_list = [2,1,7,9,4,0,23]
+my_list.sort()
+print(my_list)
+
+#Sorting strings by their aphabetical order 
+my_list = ["Cabbage","Aples","Spinach","Melons","Oranges"]
+my_list.sort()
+print(my_list)
+#Sorting by length
+my_list.sort(key=len)
+print(my_list)
+#Sorting using key value 
+my_list = [('John', 1), ('Steve', 2), ('Joe', 3)]
+
+# We can define a function for the list to sort by the second key
+
+def sort_tuple(tuple_value):
+
+    # return the key we want to sort by
+    return tuple_value[1]
+
+my_list.sort(key = sort_tuple)
+print(my_list)
+
